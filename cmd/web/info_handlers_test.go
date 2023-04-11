7c5947db6a5516d430745515f7171eb62e1a0119
Paulo <pauloa.junior@ufla.br>
2023-04-11 15:17:58 -0300

    Implementação de testes e2e

--- cmd/web/info_handlers_test.go
+++ cmd/web/info_handlers_test.go
@@ -1,35 +1,44 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/pauloa.junior/mynotes/internal/assert"
 )
 
+// func TestPing(t *testing.T) {
+// 	rr := httptest.NewRecorder()
+
+// 	r, err := http.NewRequest(http.MethodGet, "/", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	ping(rr, r)
+
+// 	rs := rr.Result()
+
+// 	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+// 	defer rs.Body.Close()
+// 	body, err := io.ReadAll(rs.Body)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	bytes.TrimSpace(body)
+
+// 	assert.Equal(t, string(body), "OK")
+// }
+
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	ping(rr, r)
+	code, _, body := ts.get(t, "/ping")
 
-	rs := rr.Result()
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
